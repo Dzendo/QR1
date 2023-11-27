bcebd288eea64489b51e4bdb0d200451487411ec
Dzendo <41204679+Dzendo@users.noreply.github.com>
2023-11-27 10:44:31 +0300

    2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.dinadurykina.qr1"
-        minSdk = 28
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
